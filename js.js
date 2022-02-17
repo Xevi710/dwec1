39c577824fd596329b253011a1d7d2e25ef0ae1a
Xevi710 <fcolpel930@g.educaand.es>
2022-02-17 20:38:47 +0100

    first commit

--- js.js
+++ js.js
@@ -1,3 +1,29 @@
 let cont = document.getElementById("cont").value;
 let otcont = document.getElementById("otcont").value;
 let email = document.getElementById("email").value;
+
+window.onload = function() {
+    var myCont = document.getElementById('cont');
+    myCont.onpaste = function(e) {
+      e.preventDefault();
+      alert("esta acción está prohibida");
+    }
+    
+    myCont.oncopy = function(e) {
+      e.preventDefault();
+      alert("esta acción está prohibida");
+    }
+  }
+
+  window.onload = function() {
+    var myCont = document.getElementById('otcont');
+    myOtCont.onpaste = function(e) {
+      e.preventDefault();
+      alert("esta acción está prohibida");
+    }
+    
+    myOtCont.oncopy = function(e) {
+      e.preventDefault();
+      alert("esta acción está prohibida");
+    }
+  }
